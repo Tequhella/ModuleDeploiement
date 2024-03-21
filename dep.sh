de532df40dd452993f5f28f2926a06dd4a5a0ae7
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2024-03-21 11:27:01 +0100

    Edit workflow

--- dep.sh
+++ dep.sh
@@ -4,12 +4,12 @@
 dpkg -s libboost-dev &> /dev/null
 if [ $? -ne 0 ]; then
     echo "Boost n'est pas installé. Installation en cours..."
-    sudo apt-get install libboost-dev
+    sudo apt-get install -y libboost-dev
 fi
 
 # Vérifiez si gtkmm est installé
 dpkg -s libgtkmm-3.0-dev &> /dev/null
 if [ $? -ne 0 ]; then
     echo "gtkmm n'est pas installé. Installation en cours..."
-    sudo apt-get install libgtkmm-3.0-dev
+    sudo apt-get install -y libgtkmm-3.0-dev
 fi
