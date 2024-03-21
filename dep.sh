c2259abcd031fc7b28e44ed93ec32d12e54efe38
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2024-03-21 12:10:21 +0100

    Add boost test framework in bash file

--- dep.sh
+++ dep.sh
@@ -3,11 +3,11 @@
 # Vérifiez si Boost est installé
 if ! dpkg-query -W -f='${Status}' libboost-dev 2>/dev/null | grep -q "install ok installed"; then
     echo "Boost n'est pas installé. Installation en cours..."
-    sudo apt-get install -y libboost-dev
+    sudo apt install -y libboost-dev libboost-test-dev
 fi
 
 # Vérifiez si gtkmm est installé
 if ! dpkg-query -W -f='${Status}' libgtkmm-3.0-dev 2>/dev/null | grep -q "install ok installed"; then
     echo "gtkmm n'est pas installé. Installation en cours..."
-    sudo apt-get install -y libgtkmm-3.0-dev
+    sudo apt install -y libgtkmm-3.0-dev
 fi
