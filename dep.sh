1adc0ec9b7f8f26dfd1be5d5a94310a5d932baff
Lilian Chardon <55657944+Tequhella@users.noreply.github.com>
2024-03-21 13:54:45 +0100

    Add gcovr install dep

--- dep.sh
+++ dep.sh
@@ -11,3 +11,9 @@ if ! dpkg-query -W -f='${Status}' libgtkmm-3.0-dev 2>/dev/null | grep -q "instal
     echo "gtkmm n'est pas installé. Installation en cours..."
     sudo apt install -y libgtkmm-3.0-dev
 fi
+
+# Vérifiez si gcovr est installé
+if ! dpkg-query -W -f='${Status}' gcovr 2>/dev/null | grep -q "install ok installed"; then
+    echo "gcovr n'est pas installé. Installation en cours..."
+    sudo apt install -y gcovr
+fi
